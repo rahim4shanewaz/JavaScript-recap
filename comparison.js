153d51dbb577760a7e9a712d83ff61d045e72a56
rahim4shanewaz <soumiq444@gmail.com>
2025-07-14 12:02:44 +0600

    revise if else

---
+++ comparison.js
@@ -0,0 +1,42 @@
+/**comparison */
+//console.log(5>10)
+
+
+
+
+
+
+var amount = 500;
+
+if (amount > 300) {
+    console.log("I will eat")
+    
+}
+else{
+    console.log()
+        "Not Gonna eat"
+}
+
+const salary = 3000;
+const bcs = true;
+const height = 61;
+
+if (salary>25000 && height>60) {
+    console.log("talented")
+    
+}
+else{
+    console.log("Need to Improve")
+}
+
+
+if (salary>25000 || height>62) {
+    console.log("talented")
+    
+}
+else{
+    console.log("Need to Improve")
+}
+   
+    
+
